487fda649654fcd48fa676c260f860534ab808fa
Ogheneyoma <Lawrenceyoma@gmail.com>
2023-09-07 15:43:52 +0100

    added getCurrentUTCTime

--- server.js
+++ server.js
@@ -13,15 +13,28 @@ app.get('/api', (req, res) => {
         "Thursday", 
         "Friday", 
         "Saturday"
-    ]
+    ];
+
+    const getCurrentUTCTime = function () {
+        const now = new Date();
+        
+        const year = now.getUTCFullYear();
+        const month = String(now.getUTCMonth() + 1).padStart(2, '0');
+        const day = String(now.getUTCDate()).padStart(2, '0');
+        const hours = String(now.getUTCHours()).padStart(2, '0');
+        const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+        const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+        
+        return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+    };
 
     const result = {
         slack_name: req.query?.slack_name, 
         current_day: daysOfTheWeek[date.getDay()],
-        utc_time: date,
+        utc_time: getCurrentUTCTime(),
         track: req.query?.track,
-        github_file_url: "", 
-        github_repo_url: "",
+        github_file_url: "https://github.com/DevYoma/HNG/blob/main/server.js", 
+        github_repo_url: "https://github.com/DevYoma/HNG",
         status_code: 200,
     }
 
